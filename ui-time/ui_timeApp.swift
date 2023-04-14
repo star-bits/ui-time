533ad8400b3e53b72d13fecc5816dfeb2529ffbf
star-bits <star-bits@outlook.com>
2023-04-15 00:38:44 +0900

    Initial Commit

---
+++ ui-time/ui_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ui_timeApp.swift
+//  ui-time
+//
+//  Created by star-bits on 2023-04-15.
+//
+
+import SwiftUI
+
+@main
+struct ui_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
